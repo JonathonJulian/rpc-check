4a47c1c22bcfbeb9be750f6a4350b7da1c03f58f
Jonathon Fritz <jonathon@blocknative.com>
2024-02-11 11:34:42 -0800

    debug

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module rpc-check
 
 go 1.20
+
+require (
+	github.com/firstrow/tcp_server v0.1.0 // indirect
+	github.com/mackerelio/go-osstat v0.2.4 // indirect
+)
--- main.go
+++ main.go
@@ -1,152 +1,50 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"io"
-	"log"
-	"net"
-	"net/http"
-	"os"
-	"strconv"
-	"strings"
-	"sync"
+	"fmt"
 	"time"
+
+	"github.com/firstrow/tcp_server"
+	"github.com/mackerelio/go-osstat/cpu"
 )
 
-type jsonRPCRequest struct {
-	JSONRPC string        `json:"jsonrpc"`
-	Method  string        `json:"method"`
-	Params  []interface{} `json:"params"`
-	ID      int           `json:"id"`
-}
-
-type jsonRPCResponse struct {
-	JSONRPC string `json:"jsonrpc"`
-	ID      int    `json:"id"`
-	Result  string `json:"result"`
-}
-
-type BlockHeightData struct {
-	LocalHeight      int64
-	HighestRefHeight int64
-	Status           string
-	Mutex            sync.RWMutex
-}
-
-var blockHeightData = BlockHeightData{}
-
-func fetchBlockHeight(nodeURL string) (int64, error) {
-	requestBody := jsonRPCRequest{
-		JSONRPC: "2.0",
-		Method:  "eth_blockNumber",
-		Params:  []interface{}{},
-		ID:      1,
-	}
-	body, err := json.Marshal(requestBody)
-	if err != nil {
-		return 0, err
-	}
-
-	resp, err := http.Post(nodeURL, "application/json", bytes.NewBuffer(body))
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-
-	responseBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return 0, err
-	}
-
-	var response jsonRPCResponse
-	if err := json.Unmarshal(responseBytes, &response); err != nil {
-		return 0, err
-	}
-
-	blockHeight, err := strconv.ParseInt(response.Result[2:], 16, 64)
-	if err != nil {
-		return 0, err
-	}
-
-	return blockHeight, nil
-}
-
-func updateBlockHeights(localNodeURL string, referenceNodeURLs []string) {
-	for {
-		localHeight, err := fetchBlockHeight(localNodeURL)
-		if err != nil {
-			log.Printf("Error fetching local node block height: %v. Marking as down.", err)
-			blockHeightData.Mutex.Lock()
-			blockHeightData.Status = "down\n"
-			blockHeightData.Mutex.Unlock()
-		} else {
-			highestHeight := int64(0)
-			for _, url := range referenceNodeURLs {
-				height, err := fetchBlockHeight(url)
-				if err != nil {
-					log.Printf("Error fetching reference node block height: %v", err)
-					continue
-				}
-				if height > highestHeight {
-					highestHeight = height
-				}
-			}
-
-			blockHeightData.Mutex.RLock()
-			log.Printf("LocalHeight: %d, HighestRefHeight: %d, Status: %s",
-				blockHeightData.LocalHeight, blockHeightData.HighestRefHeight, blockHeightData.Status)
-			blockHeightData.LocalHeight = localHeight
-			blockHeightData.HighestRefHeight = highestHeight
-			if localHeight >= highestHeight {
-				blockHeightData.Status = "up weight=100\n"
-			} else {
-				blockHeightData.Status = "up weight=50\n" // Adjust to 50% weight if out of sync
-			}
-		}
-		time.Sleep(500 * time.Millisecond) // Update every 500 milliseconds
-	}
-}
-
-func startAgentCheckServer() {
-	listenPort := os.Getenv("AGENT_LISTEN_PORT")
-	if listenPort == "" {
-		listenPort = "9876" // Default port for HAProxy agent checks
-	}
-
-	listener, err := net.Listen("tcp", ":"+listenPort)
-	if err != nil {
-		log.Fatalf("Failed to start agent check server: %v", err)
-	}
-	defer listener.Close()
-	log.Printf("Agent check server listening on :%s\n", listenPort)
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("Failed to accept connection: %v", err)
-			continue
-		}
-
-		go func(c net.Conn) {
-			defer c.Close()
-			blockHeightData.Mutex.RLock()
-			status := blockHeightData.Status
-			blockHeightData.Mutex.RUnlock()
-			println(status)
-			c.Write([]byte(status))
-		}(conn)
-	}
-}
-
 func main() {
-	localNodeURL := os.Getenv("LOCAL_NODE_URL")
-	referenceNodeURLsEnv := os.Getenv("REFERENCE_NODE_URLS")
-	if localNodeURL == "" || referenceNodeURLsEnv == "" {
-		log.Fatal("LOCAL_NODE_URL and REFERENCE_NODE_URLS environment variables must be set.")
-	}
-	referenceNodeURLs := strings.Split(referenceNodeURLsEnv, ",")
+	server := tcp_server.New(":9876")
 
-	go updateBlockHeights(localNodeURL, referenceNodeURLs)
-	startAgentCheckServer()
+	server.OnNewClient(func(c *tcp_server.Client) {
+		fmt.Println("Client connected")
+		//	cpuIdle, err := getIdleTime()
+
+		// if err != nil {
+		// 	fmt.Println(err)
+		// 	c.Close()
+		// 	return
+		// }
+
+		// if cpuIdle < 10 {
+		// 	// Set server weight to half
+		// 	c.Send("50%\n")
+		// } else {
+		// 	c.Send("100%\n")
+		// }
+		c.Send("50%\n")
+		c.Close()
+	})
+
+	server.Listen()
+}
+
+func getIdleTime() (float64, error) {
+	before, err := cpu.Get()
+	if err != nil {
+		return 0, err
+	}
+	time.Sleep(time.Duration(1) * time.Second)
+	after, err := cpu.Get()
+	if err != nil {
+		return 0, err
+	}
+	total := float64(after.Total - before.Total)
+	cpuIdle := float64(after.Idle-before.Idle) / total * 100
+	return cpuIdle, nil
 }
